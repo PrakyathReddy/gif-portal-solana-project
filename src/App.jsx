78d455baa931b10a7781d8f113644e7a5b2beed1
PrakyathReddyK <13421002-PrakyathReddyK@users.noreply.replit.com>
2023-06-03 16:18:38 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+// import React from 'react';
+// import twitterLogo from './assets/twitter-logo.svg';
+// import './App.css';
+
+// // Constants
+// const TWITTER_HANDLE = '0xBlockSmith';
+// const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+// const App = () => {
+
+//   const checkIfWalletIsConnected = async () => {
+//   // We're using optional chaining (question mark) to check if the object is null
+//     if (window?.solana?.isPhantom) {
+//       console.log('Phantom wallet found!');
+//     } else {
+//       alert('Solana object not found! Get a Phantom Wallet 👻');
+//     }
+//   };
+
+//   useEffect(() => {
+//     const onLoad = async () => {
+//       await checkIfWalletIsConnected();
+//     };
+//     window.addEventListener('load', onLoad);
+//     return () => window.removeEventListener('load', onLoad);
+//   }, []);
+  
+//   return (
+//     <div className="App">
+//       <div className="container">
+//         <div className="header-container">
+//           <p className="header">🖼 GIF Portal</p>
+//           <p className="sub-text">
+//             View your GIF collection in the metaverse here ✨
+//           </p>
+//         </div>
+//         <div className="footer-container">
+//           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+//           <a
+//             className="footer-text"
+//             href={TWITTER_LINK}
+//             target="_blank"
+//             rel="noreferrer"
+//           >{`built on @${TWITTER_HANDLE}`}</a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React, { useEffect } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Change this up to be your Twitter if you want.
+const TWITTER_HANDLE = '0xBlockSmith';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+  if (window?.solana?.isPhantom) {
+    console.log('Phantom wallet found!');
+    /*
+    * The solana object gives us a function that will allow us to connect
+    * directly with the user's wallet
+    */
+    const response = await window.solana.connect({ onlyIfTrusted: true });
+    console.log(
+      'Connected with Public Key:',
+      response.publicKey.toString()
+    );
+  } else {
+    alert('Solana object not found! Get a Phantom Wallet 👻');
+  }
+};
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection in the metaverse ✨
+          </p>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
